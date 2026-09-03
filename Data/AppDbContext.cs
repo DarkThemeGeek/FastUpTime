7d7abe56b8c0f1e7b29c1dc607600ce24aed5d03
DarkThemeGeek <imperator09@atomicmail.io>
2026-09-03 14:08:14 +0300

    Added the relationship for the joint table

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -13,4 +13,20 @@ public class AppDbContext : DbContext
     public DbSet<UserAccount> UserAccounts { get; set; }
     public DbSet<Site> Sites { get; set; }
     public DbSet<SitePing> SitePings { get; set; }
+    public DbSet<UserAccountSite> UserAccountSites { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserAccountSite>()
+            .HasKey(x => new { x.UserAccountId, x.SiteId });
+
+        modelBuilder.Entity<UserAccountSite>()
+            .HasOne(x => x.UserAccount)
+            .WithMany(x=>x.Sites)
+            .HasForeignKey(x => x.UserAccountId);
+
+        modelBuilder.Entity<UserAccountSite>()
+            .HasOne(x => x.Site)
+            .WithMany(x=>x.Accounts)
+            .HasForeignKey(x => x.SiteId);
+    }
 }
